864e8064d268335b90a9e51e0c8088f1b508f4b9
Joseph Lee <jlee01@stuy.edu>
2019-11-03 18:30:46 -0500

    hottest main of all time

--- musicalLibraryFunctions.c
+++ musicalLibraryFunctions.c
@@ -13,134 +13,32 @@
 //};
 
 void print_library(struct song_node *p){
-  print_library(p);
-}
-struct song_node * add_song_alphabet(struct song_node *p, char song_name[100], char artist_name[100]){
-  struct song_node *pnew;
-  struct song_node *looper=p;
-  struct song_node *recorder=NULL;
-  pnew=malloc(sizeof(struct song_node));
-  strcpy(pnew->name,song_name);
-  strcpy(pnew->artist,artist_name);
-  while(looper !=NULL){
-    //printf("%d\n",strcmp(looper->artist,artist_name) );
-    if(strcmp(looper->artist,artist_name)>0){
-      if(recorder==NULL){
-        pnew->next=looper;
-        //printf("%s\n",pnew->next->artist);
-        p=pnew;
-        return pnew;
-      }else{
-        connectNodes(recorder,pnew,looper);
-        return p;
-      }
-    }
-    if(strcmp(looper->artist,artist_name)==0){
-      if(strcmp(looper->name,song_name)>0){
-        if(recorder==NULL){
-          pnew->next=looper;
-          return pnew;
-        }else{
-          connectNodes(recorder,pnew,looper);
-          return p;
-        }
-      }else{
-        connectNodes(looper,pnew,looper->next);
-        return p;
-      }
-    }
-    recorder=looper;
-    looper=looper->next;
-  }
-  if(recorder!=NULL){
-    //printf("whats");
-    recorder->next=pnew;
-    return p;
-  }
-  return pnew;
-}
-void connectNodes(struct song_node *p,struct song_node *q,struct song_node *r){
-  p->next=q;
-  q->next=r;
 }
 struct song_node * add_song(struct song_node *p, char song_name[100], char artist_name[100]){
-  struct song_node *pnew;
-  pnew=malloc(sizeof(struct song_node));
-  strcpy(pnew->name,song_name);
-  strcpy(pnew->artist,artist_name);
-  pnew->next=p;
-  return pnew;
 }
 
 struct song_node * find_song(struct song_node *p,char song_name[100], char artist_name[100]){
-  struct song_node *looper=p;
-  while(looper !=NULL){
-    if(strcmp(looper->artist,artist_name) == 0 && strcmp(looper->name,song_name) == 0){
-      return looper;
-    }
-    looper=looper->next;
-  }
-  return looper;
 }
 struct song_node * find_first_song(struct song_node *p,char artist_name[100]){
-  struct song_node *looper=p;
-  while(looper !=NULL){
-    if(strcmp(looper->artist,artist_name) == 0){
-      return looper;
-    }
-    looper=looper->next;
-  }
-  return looper;
+
 }
 
 
-struct song_node * remove_song (struct song_node *library, char song_name[100], char artist_name[100]){
-  struct song_node * current_song = library;
-  struct song_node * placeholder;
-  if(strcmp(current_song->name,song_name) == 0 && strcmp(current_song->artist,artist_name) == 0){ //if I have to remove front node, it's a special case
-    library = library->next;
-    free(current_song);
-    return library;
+struct song_node * remove_song (struct song_node * library[27], char song_name[100], char artist_name[100]){
+  if(artist_name[0] - 97 < 0){
+    remove_node(library[26], song_name, artist_name)
   }
-  while(current_song->next != NULL){ //else, loop through, checking if my next node has to be removed
-    if (strcmp(current_song->next->name,song_name) == 0 && strcmp(current_song->next->artist,artist_name) == 0){ //if it does have to be removed, relink neccesary nodes, and free node
-      placeholder = current_song->next;
-      current_song->next = current_song->next->next;
-      free(placeholder);
-      return library;
-    } else{ //if not, move on to next node
-      current_song = current_song->next;
-    }
-  }
-  return library;
+  remove_node(library[artist[0] - 97], song_name, artist_name)
 }
 
-struct song_node * free_library(struct song_node* library){
-  struct song_node* sub;
-  while (library!=NULL) {
-    sub=library;
-    printf("Freeing: %s, %s\n", library->name, library->artist);
-    library=library->next;
-    free(sub);
+struct song_node * free_library(struct song_node * library[27]){
+  int i = 0;
+  for(i = 0; i < 27; i++){
+    free_list(library[0]);
   }
-  return library;
 }
 
 struct song_node * random_song(struct song_node *library, int random){
-  struct song_node *looper=library;
-  int size=0;
-  while(looper != NULL){
-      size++;
-      looper = looper->next;
-    }
-  random = random%size;
-  int count;
-  looper=library;
-  for(count = 0;looper != NULL;count++){
-    if(count == random){
-      return looper;
-    }
-    looper=looper->next;
-  }
-  return library;
+  random = random%27;
+  return random_song(library[random]);
 }
